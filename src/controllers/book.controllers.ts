fb9e02aac412de9e2c892d5997eeac4cdc028d20
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-06-26 01:23:12 +0600

    created getAllBooks function

--- src/controllers/book.controllers.ts
+++ src/controllers/book.controllers.ts
@@ -24,3 +24,35 @@ export const createBook = async (req: Request, res: Response) => {
       });
   }
 };
+
+// get books
+export const getAllBooks = async (req: Request, res: Response) => {
+  // ?filter=FANTASY&sortBy=createdAt&sort=desc&limit=5
+  try {
+    const { filter, sortBy = "createdAt", sort = "desc", limit } = req.query;
+    // eslint-disable-next-line
+    const query: Record<string, any> = {};
+
+    if (filter) query.genre = filter;
+    const SOrder = sort === "asc" ? 1 : -1;
+    const limitNum = parseInt(limit as string) || 10;
+
+    const result = await Book.find(query)
+      .sort({ [sortBy as string]: SOrder })
+      .limit(limitNum);
+
+    apiResponse(res, 200, true, "Books retrieved successfully", result);
+    // eslint-disable-next-line
+  } catch (error: any) {
+    if (error.name === "ValidationError") {
+      errorResponse(res, 400, "Validation failed", {
+        name: error.name,
+        errors: error.errors,
+      });
+    } else
+      errorResponse(res, 500, "Internal server error", {
+        name: error.name,
+        message: error.message,
+      });
+  }
+};
