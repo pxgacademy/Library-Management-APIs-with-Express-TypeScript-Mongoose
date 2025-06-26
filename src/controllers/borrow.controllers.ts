fdea3fb807d20efbf5e2ba769a3ecd81d22233a2
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-06-26 13:12:57 +0600

    created getSummary function

--- src/controllers/borrow.controllers.ts
+++ src/controllers/borrow.controllers.ts
@@ -24,3 +24,53 @@ export const createBorrow = async (req: Request, res: Response) => {
       });
   }
 };
+
+export const getSummary = async (req: Request, res: Response) => {
+  try {
+    const result = await Borrow.aggregate([
+      { $group: { _id: "$book", totalQuantity: { $sum: "$quantity" } } },
+      {
+        $lookup: {
+          from: "books",
+          localField: "_id",
+          foreignField: "_id",
+          as: "bookInfo",
+        },
+      },
+      {
+        $unwind: "$bookInfo",
+      },
+      {
+        $project: {
+          _id: 0,
+          totalQuantity: 1,
+          book: {
+            title: "$bookInfo.title",
+            isbn: "$bookInfo.isbn",
+          },
+        },
+      },
+    ]);
+
+    apiResponse(
+      res,
+      200,
+      true,
+      "Borrowed books summary retrieved successfully",
+      result
+    );
+
+    // eslint-disable-next-line
+  } catch (error: any) {
+    if (error.name === "ValidationError") {
+      errorResponse(res, 400, "Validation failed", {
+        name: error.name,
+        errors: error.errors,
+      });
+    } else
+      errorResponse(res, 500, "Internal server error", {
+        name: error.name,
+        message: error.message,
+      });
+  }
+};
